49af955ccca5397b1b6efb2013ca88395a74e5ca
morz-mamun <morzmamun@gmail.com>
2023-10-20 04:00:03 +0600

    Add product data in mongoDB and server site

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,7 +12,7 @@ app.use(express.json())
 //*** mongoDB code  ***//
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttjbmkn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +30,21 @@ async function run() {
         await client.connect();
 
         const productCollection = client.db("TechnologyDB").collection("Products")
+
+        // add product related API 
+        app.get('/products', async(req, res) => {
+            const cursor = productCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/products', async(req, res) => {
+            const newProduct = req.body
+            const result = await productCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
+        
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
